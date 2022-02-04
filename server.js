a9a4b357acba6f27e2f398e79561abba4d0c7426
CristinismN <chrystyna_n@yahoo.com>
2022-02-04 16:05:10 +0200

    adding port

--- server.js
+++ server.js
@@ -73,8 +73,8 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3001, ()=> { // this function will run right after the listen on port 3000 happens
-    console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3001, ()=> { // this function will run right after the listen on port 3000 happens
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
 
